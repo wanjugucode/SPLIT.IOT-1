4fe7776d52f3c987ccdd2562d713d781e31dfaff
wanjugucode <79141887+wanjugucode@users.noreply.github.com>
2021-04-23 21:07:10 +0300

    Add files via upload

---
+++ 1ST_SPLIT.ino
@@ -0,0 +1,18 @@
+int Red=12;
+int Blue=11;
+int Green=10;
+
+void setup(){
+
+            SETUP();
+  }
+
+void loop(){
+        
+        RED();
+        BLUE();
+        GREEN();
+
+
+  
+           }            
